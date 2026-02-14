077c21e031412164317abafb91862da5b109610d
Avishek Kait <avishekkait@gmail.com>
2026-02-14 18:09:55 +0600

    A. A+B Again?

---
+++ 1999A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    int a = n%10;
+    int b = n/10;
+    int sum = a+b;
+    cout << sum <<endl ;
+}
+
+int main() {
+    
+     long long test;
+     cin >> test;
+     while(test--)
+     {
+        solve();
+     }
+    
+    return 0;
+}
