78f9ff8cd4427653da9ffaf1034555382b659064
Avishek Kait <avishekkait@gmail.com>
2026-02-14 18:54:03 +0600

    A. Minimize!

---
+++ 2009A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a,b;
+    cin >> a >> b;
+    int r = b-a;
+    cout << r << endl;
+}
+
+int main() {
+    
+     long long test;
+     cin >> test;
+     while(test--)
+     {
+        solve();
+     }
+    
+    return 0;
+}
