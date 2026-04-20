3673a2a208c27153d697e4b47b22f0ec91237244
Avishek Kait <avishekkait@gmail.com>
2026-04-21 00:12:07 +0600

    A. Marathon

---
+++ 1692A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n,c=0;
+cin >> n;
+while(n--){
+vector<int>a(4);
+for(auto &i : a)
+{
+cin >> i ;
+
+}
+for(int i =1 ; i<4 ; i++){
+if (a[0]<  a[i]){
+c++;
+}
+
+}
+cout << c <<endl;
+c=0;}
+
+return 0;
+}
